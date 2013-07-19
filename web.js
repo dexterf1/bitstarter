c721f0852717ab50ba0d646455ca68cb1059baaa
Dexter Ferreira <dexterf@gmail.com>
2013-07-18 17:55:32 -0700

    modified web.js

--- web.js
+++ web.js
@@ -1,9 +1,17 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  
+  fs.readFileSync('index.html');
+  
+  var buffer = fs.readFileSync('index.html');
+
+  response.send(buffer.toString());
+
+ 
 });
 
 var port = process.env.PORT || 5000;
